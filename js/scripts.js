da12bc87b46cba91292013eaf96f36329e0ebd10
Grace Mekarski <gracelauren909@gmail.com>
2015-02-13 14:48:52 -0800

    working animation

--- js/scripts.js
+++ js/scripts.js
@@ -33,9 +33,30 @@ $(document).ready(function() {
     var sideB = $("input#sideB").val();
     var sideC = $("input#sideC").val();
     var sideLengths = sideA + sideB + sideC;
+    var longestSide = sideLengths.split("").sort().pop();
     var result = triangleType(sideLengths);
     $(".type").text(result);
     $("#result").show();
+
+    if( parseInt(sideA) === parseInt(longestSide) && parseInt(longestSide) === parseInt(sideB) ) {
+      $('#ttype').switchClass('triangle','equilateral', "easeInOutQuad");
+    } else if( parseInt(sideA) === parseInt(longestSide) || parseInt(longestSide) === parseInt(sideB) || parseInt(sideB) === parseInt(sideA) ) {
+      $('#ttype').switchClass('triangle','isosceles', "easeInOutQuad");
+    } else {
+      $('#ttype').switchClass('triangle','scalene', "easeInOutQuad");
+    }
+
     event.preventDefault();
   });
+  //
+  // $("form#sides").submit(function(event) {
+  //     $('triangle').switchClass('triangle','scalene');
+  //     if( parseInt(sideA) === parseInt(longestSide) && parseInt(longestSide) === parseInt(sideB) ) {
+  //       $('triangle').switchClass('triangle','equilateral');
+  //     } else if( parseInt(sideA) === parseInt(longestSide) || parseInt(longestSide) === parseInt(sideB) || parseInt(sideB) === parseInt(sideA) ) {
+  //       $('triangle').switchClass('triangle','isosceles');
+  //     }
+  // });
+
+
 });
